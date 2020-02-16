64d29d96abd1a7cef765e26eaa0ed2cc62623c10
Mote1337 <45979009+Mote1337@users.noreply.github.com>
2020-02-16 14:34:07 +0100

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,8 +1,8 @@
-TOKEN=0l6X5sdBvK3Pwo0Q1-kHy53ROTVqBVeNQUnJ3vXPR_M
-ENDPOINT=master.svil2.ocprm.testposte
+TOKEN=xxxxx
+ENDPOINT=xxxxx
 PORT=8443
-NAMESPACE=frontendlibretti
-NOME_DC_1=libretti-aperture-associazioneruoli
+NAMESPACE=xxxxxx
+NOME_DC_1=xxxxx
 PRM="?export"
 
 curl -k \
