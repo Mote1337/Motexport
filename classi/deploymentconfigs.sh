09f5098104fef38465a477d08be68f5e62c1db3c
Mote1337 <motevonsen@gmail.com>
2020-02-16 03:36:30 +0100

    jq

--- classi/deploymentconfigs.sh
+++ classi/deploymentconfigs.sh
@@ -30,14 +30,14 @@ report_deploymentconfig(){
 
 	containers=( $(oc get --export -o json deploymentconfigs/libretti-aperture-associazioneruoli | jq '.spec.template.spec.containers[] .name') )
 
-	 echo "Mi ritornano 2 valori da qui? ${#containers[@]}, VALORE 1: ${containers[0]} ---- VALORE2: ${containers[1]}"
+	 echo "Mi ritornano 2 valori da qui? ${#containers[@]}, VALORE 1: ${containers[0]} ---- VALORE2: ${containers[1]} --------- "
 	if [ ${#dc[@]} -gt 0 ]
 		then
 		{
 			echo "Esistono [${#dc[@]}] Deployment Config in $1"
 			# Ciclo for per leggere il DC, quanti Container i Limit e Requests
 			n=0
-			for i in $(oc get deploymentconfigs -o name -n $1);do let n=$n+1; echo "Il [$n] Deployment Config si chiama $i"; echo "Il Deployment Config è ${#containers[@]} Containers";echo ${containers[0]};done
+			for i in $(oc get deploymentconfigs -o name -n $1);do let n=$n+1; echo "Il [$n] Deployment Config si chiama $i"; echo "Il Deployment Config è ${#containers[@]} Containers";echo ${containers[@]};done
 		}
 		else
 		{
